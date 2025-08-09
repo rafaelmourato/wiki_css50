80623997410c5cd097026afd92ec887f9c9e1a63
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-08-09 14:41:02 -0300

    Finishing the specification of the entry and index pages the simplest way, will make it better later

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.utils._os import safe_join
+from django.http import HttpResponse
 
 from . import util
 
@@ -7,4 +10,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+entries =util.list_entries()
+
+def page(request, page):
+    if page in entries:
+        finalPage = util.get_entry(page)
+        return HttpResponse(finalPage)
+    else:
+        notfound = util.get_entry("NotFound")
+        return HttpResponse(notfound)
+
 
