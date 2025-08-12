c5a68e403798164b6c93226ff32f4397b8309b3c
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-08-12 14:39:58 -0300

    Implemting the search protocol. Also going to try to use lower() function to avoid CSS beeing diferente than Css or CSs

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.utils._os import safe_join
 from django.http import HttpResponse
-
 from . import util
 
+entries =util.list_entries()
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-entries =util.list_entries()
+    if request.method == "GET" and request.GET.get("q") != None and request.GET.get("q") != '':
+        search = request.GET.get("q")
+        if search in entries:
+            return redirect('page', page=search)
+        else:
+            matched_entries = [entry for entry in entries if search.lower() in entry.lower()]
+            return render(request, "encyclopedia/search.html", {
+            "entries": matched_entries,
+            "search": search
+            })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def page(request, page):
     if page in entries:
